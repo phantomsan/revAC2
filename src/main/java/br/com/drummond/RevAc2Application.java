97f3fb5342d5c726dc31e3f32c46a3eb5e6d501b
phantomsan <ricardoiva9@gmail.com>
2019-10-09 21:41:29 -0300

    Commitado!

---
+++ src/main/java/br/com/drummond/RevAc2Application.java
@@ -0,0 +1,13 @@
+package br.com.drummond;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RevAc2Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(RevAc2Application.class, args);
+	}
+
+}
